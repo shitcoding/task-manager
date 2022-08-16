ecc98aaf3b9c0a895a9cb166175953f60129f10a
shitcoding <shitcoder@pm.me>
2022-08-16 13:17:21 +0300

    add loading of env variables from .env file

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+
 import os
 import sys
+
+from dotenv import load_dotenv
 
 
 def main():
@@ -19,4 +21,5 @@ def main():
 
 
 if __name__ == "__main__":
+    load_dotenv()  # Loading env variables from .env file
     main()
