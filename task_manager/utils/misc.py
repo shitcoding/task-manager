6f569eb48407351036983fafb006fd1ddf27a27d
shitcoding <shitcoder@pm.me>
2022-08-17 16:01:31 +0300

    add utils module with function retrieving SECRET_KEY from env variable

---
+++ task_manager/utils/misc.py
@@ -0,0 +1,15 @@
+import os
+
+from django.core.exceptions import ImproperlyConfigured
+
+
+def getenv(env_variable):
+    """
+    Return an environment variable or raise an exception.
+    """
+    try:
+        return os.environ[env_variable]
+    except KeyError:
+        raise ImproperlyConfigured(
+            f'The {env_variable} setting must not be empty.',
+        )
