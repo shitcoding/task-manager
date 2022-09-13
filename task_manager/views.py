91ac4ca0ca118a9a4b0c5b32dcb0e72f7ddef6d5
shitcoding <shitcoder@pm.me>
2022-09-13 14:18:36 +0300

    add basic views and routes

---
+++ task_manager/views.py
@@ -0,0 +1,89 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import User, Task, Label, Status
+
+
+def index(request):
+    return render(request, 'task_manager/index.html')
+
+
+# Users
+def login(request):
+    return HttpResponse("You're at login page")
+
+
+def signup(request):
+    return HttpResponse("You're at signup page")
+
+
+def logout(request):
+    return HttpResponse("You're at logout page")
+
+
+def users(request):
+    return render(request, 'task_manager/users.html')
+
+
+def user_update(request, user_id):
+    return HttpResponse(f"You're at user {user_id} update page")
+
+
+def user_delete(request, user_id):
+    return HttpResponse(f"You're at user {user_id} delete page")
+
+
+# Tasks
+def tasks(request):
+    return HttpResponse("You're at the tasks list page")
+
+
+def task_create(request):
+    return HttpResponse("You're at the task creation page")
+
+
+def task_detail(request, task_id):
+    return HttpResponse(f"You're at task {task_id} details page")
+
+
+def task_update(request, task_id):
+    return HttpResponse(f"You're at task {task_id} update page")
+
+
+def task_delete(request, task_id):
+    return HttpResponse(f"You're at task {task_id} delete page")
+
+
+# Statuses
+def statuses(request):
+    return HttpResponse("You're at the statuses list page")
+
+
+def status_create(request):
+    return HttpResponse("You're at the status creation page")
+
+
+def status_update(request, status_id):
+    return HttpResponse(f"You're at status {status_id} update page")
+
+
+def status_delete(request, status_id):
+    return HttpResponse(f"You're at status {status_id} delete page")
+
+
+# Labels
+def labels(request):
+    return HttpResponse("You're at the labels list page")
+
+
+def label_create(request):
+    return HttpResponse("You're at the label creation page")
+
+
+def label_update(request, label_id):
+    return HttpResponse(f"You're at label {label_id} update page")
+
+
+def label_delete(request, label_id):
+    return HttpResponse(f"You're at label {label_id} delete page")
+
