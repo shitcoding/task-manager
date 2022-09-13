852bcbf04689513c9503f3c15aa3261b553fdac1
shitcoding <shitcoder@pm.me>
2022-09-13 14:25:47 +0300

    implement i18n localization

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -134,11 +134,21 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
+LANGUAGES = [
+        ('en', 'English'),
+        ('ru', 'Russian'),
+]
+
+LOCALE_PATHS = [
+        BASE_DIR / 'locale',
+]
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
