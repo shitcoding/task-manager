33beb17a4037d9903ab46268b7d5caedc61ad037
shitcoding <shitcoder@pm.me>
2022-09-15 18:01:25 +0300

    register User model in admin panel

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import User, Task, Status, Label
 
 
+admin.site.register(User)
 admin.site.register(Task)
 admin.site.register(Status)
 admin.site.register(Label)
