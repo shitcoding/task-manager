311f1f13725a0fa607eb5e32b759449bc930f05a
shitcoding <shitcoder@pm.me>
2022-09-17 19:48:30 +0300

    add list views for all models, add task detail view, replace all views with class based

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,100 +1,161 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
-from django.views import generic
+from django.views import generic, View
 
 from .models import User, Task, Label, Status
 
 
-def index(request):
-    return render(request, "task_manager/index.html")
+class IndexView(generic.TemplateView):
+    """Index page view."""
+
+    template_name = "task_manager/index.html"
 
 
 # Users
-def login(request):
-    return HttpResponse("You're at login page")
+class LoginView(View):
+    """Login page view."""
+
+    def get(self, request):
+        return HttpResponse("You're at login page")
 
 
-def signup(request):
-    return HttpResponse("You're at signup page")
+class SignupView(View):
+    """Signup page view."""
+
+    def get(self, request):
+        return HttpResponse("You're at signup page")
 
 
-def logout(request):
-    return HttpResponse("You're at logout page")
+class LogoutView(View):
+    """Logout page view."""
+
+    def get(self, request):
+        return HttpResponse("You're at logout page")
 
 
 class UserListView(generic.ListView):
-    template_name = "task_manager/users.html"
-    context_object_name = "users_list"
+    template_name = "task_manager/user_list.html"
+    context_object_name = "user_list"
 
     def get_queryset(self):
         """Return the list of all registered users."""
         return User.objects.order_by("-signup_date")
 
 
-def user_update(request, user_id):
-    return HttpResponse(f"You're at user {user_id} update page")
+class UserUpdateView(View):
+    """User update page view."""
+
+    def get(self, request, user_id):
+        return HttpResponse(f"You're at user {user_id} update page")
 
 
-def user_delete(request, user_id):
-    return HttpResponse(f"You're at user {user_id} delete page")
+class UserDeleteView(View):
+    """User delete page view."""
+
+    def get(self, request, user_id):
+        return HttpResponse(f"You're at user {user_id} delete page")
 
 
 # Tasks
 class TaskListView(generic.ListView):
-    template_name = "task_manager/tasks.html"
-    context_object_name = "tasks_list"
+    template_name = "task_manager/task_list.html"
+    context_object_name = "task_list"
 
     def get_queryset(self):
         """Return the list of all tasks."""
         return Task.objects.order_by("-created_on")
 
 
-def task_create(request):
-    return HttpResponse("You're at the task creation page")
+class TaskCreateView(View):
+    """Task creation page view."""
+
+    def get(self, request):
+        return HttpResponse("You're at the task creation page")
 
 
-def task_detail(request, task_id):
-    return HttpResponse(f"You're at task {task_id} details page")
+class TaskDetailView(generic.DetailView):
+    """Task detail page view."""
+
+    model = Task
+    template_name = "task_manager/task_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
 
-def task_update(request, task_id):
-    return HttpResponse(f"You're at task {task_id} update page")
+class TaskUpdateView(View):
+    """Task update page view."""
+
+    def get(self, request, task_id):
+        return HttpResponse(f"You're at task {task_id} update page")
 
 
-def task_delete(request, task_id):
-    return HttpResponse(f"You're at task {task_id} delete page")
+class TaskDeleteView(View):
+    """Task delete page view."""
+
+    def get(self, request, task_id):
+        return HttpResponse(f"You're at task {task_id} delete page")
 
 
 # Statuses
-def statuses(request):
-    return HttpResponse("You're at the statuses list page")
+class StatusListView(generic.ListView):
+    template_name = "task_manager/status_list.html"
+    context_object_name = "status_list"
+
+    def get_queryset(self):
+        """Return the list of all statuses."""
+        return Status.objects.order_by("-created_on")
 
 
-def status_create(request):
-    return HttpResponse("You're at the status creation page")
+class StatusCreateView(View):
+    """Status creation page view."""
+
+    def get(self, request):
+        return HttpResponse("You're at the status creation page")
 
 
-def status_update(request, status_id):
-    return HttpResponse(f"You're at status {status_id} update page")
+class StatusUpdateView(View):
+    """Status update page view."""
+
+    def get(self, request, status_id):
+        return HttpResponse(f"You're at status {status_id} update page")
 
 
-def status_delete(request, status_id):
-    return HttpResponse(f"You're at status {status_id} delete page")
+class StatusDeleteView(View):
+    """Status delete page view."""
+
+    def get(self, request, status_id):
+        return HttpResponse(f"You're at status {status_id} delete page")
 
 
 # Labels
-def labels(request):
-    return HttpResponse("You're at the labels list page")
+class LabelListView(generic.ListView):
+    template_name = "task_manager/label_list.html"
+    context_object_name = "label_list"
+
+    def get_queryset(self):
+        """Return the list of all labels."""
+        return Label.objects.order_by("-created_on")
 
 
-def label_create(request):
-    return HttpResponse("You're at the label creation page")
+class LabelCreateView(View):
+    """Label creation page view."""
+
+    def get(self, request):
+        return HttpResponse("You're at the label creation page")
 
 
-def label_update(request, label_id):
-    return HttpResponse(f"You're at label {label_id} update page")
+class LabelUpdateView(View):
+    """Label update page view."""
+
+    def get(self, request, label_id):
+        return HttpResponse(f"You're at label {label_id} update page")
 
 
-def label_delete(request, label_id):
-    return HttpResponse(f"You're at label {label_id} delete page")
+class LabelDeleteView(View):
+    """Label delete page view."""
+
+    def get(self, request, label_id):
+        return HttpResponse(f"You're at label {label_id} delete page")
