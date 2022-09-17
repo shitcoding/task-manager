587a8706724b5df64fc1927d2c2c90c4cfc40f93
shitcoding <shitcoder@pm.me>
2022-09-17 19:48:30 +0300

    add list views for all models, add task detail view, replace all views with class based

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -5,45 +5,72 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index, name="index"),
-    path("login/", views.login, name="login"),
+    path("", views.IndexView.as_view(), name="index"),
+    # Auth
+    path("login/", views.LoginView.as_view(), name="login"),
     path("accounts/", include("django.contrib.auth.urls")),  # login test
-    path("logout/", views.logout, name="logout"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
     # Users
-    path("users/create/", views.signup, name="signup"),
+    path("users/create/", views.SignupView.as_view(), name="signup"),
     path("users/", views.UserListView.as_view(), name="users"),
-    path("users/<int:user_id>/update/", views.user_update, name="user_update"),
-    path("users/<int:user_id>/delete/", views.user_delete, name="user_delete"),
+    path(
+        "users/<int:user_id>/update/",
+        views.UserUpdateView.as_view(),
+        name="user_update",
+    ),
+    path(
+        "users/<int:user_id>/delete/",
+        views.UserDeleteView.as_view(),
+        name="user_delete",
+    ),
     # Tasks
     path("tasks/", views.TaskListView.as_view(), name="tasks"),
-    path("tasks/create/", views.task_create, name="task_create"),
-    path("tasks/<int:task_id>/", views.task_detail, name="task_detail"),
-    path("tasks/<int:task_id>/update/", views.task_update, name="task_update"),
-    path("tasks/<int:task_id>/delete/", views.task_delete, name="task_delete"),
+    path("tasks/create/", views.TaskCreateView.as_view(), name="task_create"),
+    path(
+        "tasks/<int:pk>/",
+        views.TaskDetailView.as_view(),
+        name="task_detail",
+    ),
+    path(
+        "tasks/<int:task_id>/update/",
+        views.TaskUpdateView.as_view(),
+        name="task_update",
+    ),
+    path(
+        "tasks/<int:task_id>/delete/",
+        views.TaskDeleteView.as_view(),
+        name="task_delete",
+    ),
     # Statuses
-    path("statuses/", views.statuses, name="statuses"),
-    path("statuses/create/", views.status_create, name="status_create"),
+    path("statuses/", views.StatusListView.as_view(), name="statuses"),
+    path(
+        "statuses/create/",
+        views.StatusCreateView.as_view(),
+        name="status_create",
+    ),
     path(
         "statuses/<int:status_id>/update/",
-        views.status_update,
+        views.StatusUpdateView.as_view(),
         name="status_update",
     ),
     path(
         "statuses/<int:status_id>/delete/",
-        views.status_delete,
+        views.StatusDeleteView.as_view(),
         name="status_delete",
     ),
     # Labels
-    path("labels/", views.labels, name="labels"),
-    path("labels/create/", views.label_create, name="label_create"),
+    path("labels/", views.LabelListView.as_view(), name="labels"),
+    path(
+        "labels/create/", views.LabelCreateView.as_view(), name="label_create"
+    ),
     path(
         "labels/<int:label_id>/update/",
-        views.label_update,
+        views.LabelUpdateView.as_view(),
         name="label_update",
     ),
     path(
         "labels/<int:label_id>/delete/",
-        views.label_delete,
+        views.LabelDeleteView.as_view(),
         name="label_delete",
     ),
 ]
