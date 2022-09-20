324273579b79fe99b61342f922e6aa4ba7c932d3
shitcoding <shitcoder@pm.me>
2022-09-21 00:17:35 +0300

    remove login page route used for testing

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
@@ -9,7 +9,6 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     # Auth
     path("login/", views.LoginView.as_view(), name="login"),
-    path("accounts/", include("django.contrib.auth.urls")),  # login test
     path("logout/", views.LogoutView.as_view(), name="logout"),
     # Users
     path("users/create/", views.SignupView.as_view(), name="signup"),
