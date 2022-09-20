ca9f25df62e27570ca54b723f562d6a75d35c845
shitcoding <shitcoder@pm.me>
2022-09-20 18:15:54 +0300

    set custom AUTH_USER_MODEL in settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -76,6 +76,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "task_manager.wsgi.application"
 
+# Authentication
+
+AUTH_USER_MODEL = "task_manager.SiteUser"
+
+LOGIN_REDIRECT_URL = "index"
+
+LOGOUT_REDIRECT_URL = "index"
+
 # Database settings
 DATABASES = {
     "default": {
@@ -116,13 +124,21 @@ AUTH_PASSWORD_VALIDATORS = [
         ),
     },
     {
-        "NAME": ("django.contrib.auth.password_validation." "MinimumLengthValidator"),
+        "NAME": (
+            "django.contrib.auth.password_validation." "MinimumLengthValidator"
+        ),
     },
     {
-        "NAME": ("django.contrib.auth.password_validation." "CommonPasswordValidator"),
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "CommonPasswordValidator"
+        ),
     },
     {
-        "NAME": ("django.contrib.auth.password_validation." "NumericPasswordValidator"),
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "NumericPasswordValidator"
+        ),
     },
 ]
 
