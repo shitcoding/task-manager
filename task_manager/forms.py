29b3ae17300a14cb332ec86aef9a14c820718a25
shitcoding <shitcoder@pm.me>
2022-09-20 18:16:39 +0300

    add user creation and user change forms

---
+++ task_manager/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import forms
+from .models import SiteUser
+
+
+class UserCreationForm(forms.UserCreationForm):
+    """Task manager user creation form."""
+
+    class Meta(forms.UserCreationForm.Meta):
+        model = SiteUser
+        fields = ["username", "first_name", "last_name"]
+
+
+class UserChangeForm(forms.UserChangeForm):
+    """Task manager user change form."""
+
+    class Meta(forms.UserChangeForm.Meta):
+        model = SiteUser
+        fields = ["username", "first_name", "last_name"]
