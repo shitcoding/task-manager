0cb400eb98e84d4199d284a9dbcd40aae35144ff
shitcoding <shitcoder@pm.me>
2022-10-19 17:00:28 +0300

    refactor server responses tests

--- task_manager/tests/test_server_responses.py
+++ task_manager/tests/test_server_responses.py
@@ -1,15 +1,25 @@
 import pytest
-from django.urls import reverse_lazy
+from django.urls import reverse
+
+from task_manager.tests.fixtures import (
+    auto_login_user,
+    create_label,
+    create_status,
+    create_task,
+    create_user,
+    test_password,
+)
 
 
 def test_index_page(client):
     """Test loading index page."""
-    response = client.get(reverse_lazy("index"))
+    response = client.get(reverse("index"))
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
 def test_user_list_page(client):
     """Test loading users list page."""
-    response = client.get(reverse_lazy("users"))
+    response = client.get(reverse("users"))
+    assert response.status_code == 200
     assert response.status_code == 200
