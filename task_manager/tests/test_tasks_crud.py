e2650819d7de9e9391dd58721ef7353e20954cc7
shitcoding <shitcoder@pm.me>
2022-10-21 00:08:03 +0300

    add test for updating the task

--- task_manager/tests/test_tasks_crud.py
+++ task_manager/tests/test_tasks_crud.py
@@ -72,10 +72,63 @@ def test_task_creation(
     assert str(task.status) in tasks_list_content
     assert str(task.creator) in tasks_list_content
     assert str(task.performer) in tasks_list_content
+
+
+def test_task_update(
+    client,
+    create_task,
+    create_user,
+    create_status,
+    create_labels_set,
+    faker,
+    auto_login_user,
+):
+    """
+    Test updating task details.
+
+    Should change task details, redirect to tasks list page
+    and show success flash message.
+    """
+    task = create_task()
+
+    new_name = faker.pystr(min_chars=10, max_chars=15)
+    new_description = faker.paragraph(nb_sentences=5)
+    new_performer = create_user()
+    new_status = create_status()
+    new_labels = create_labels_set()
+
+    data = {
+        "name": new_name,
+        "description": new_description,
+        "status": new_status.pk,
+        "performer": new_performer.pk,
+        "label": [label.pk for label in new_labels],
+    }
+
     client, _ = auto_login_user()
-    response = client.get(reverse("tasks"))
-    page_content = str(response.content)
-    assert task.name in page_content
-    assert str(task.status) in page_content
-    assert str(task.creator) in page_content
-    assert str(task.performer) in page_content
+    response = client.post(
+        reverse("task_update", kwargs={"pk": task.pk}),
+        data,
+    )
+    # Should redirect to tasks list page
+    assert response.status_code == 302
+    assert response.url == reverse("tasks")
+    # Should show success flash message after redirect
+    redirect_response = client.get(response.url)
+    assert "Task updated successfully" in str(
+        redirect_response.content,
+    )
+    # Task details should be changed successfully
+    updated_task = Task.objects.get(pk=task.pk)
+    assert updated_task.name == new_name
+    assert updated_task.description == new_description
+    assert updated_task.performer == new_performer
+    assert updated_task.status == new_status
+    assert updated_task.label.count() == len(new_labels)
+    assert list(updated_task.label.all()) == new_labels
+    # Created task should be shown on the tasks list page
+    tasks_list_content = str(redirect_response.content)
+    assert updated_task.name in tasks_list_content
+    assert str(updated_task.status) in tasks_list_content
+    assert str(updated_task.creator) in tasks_list_content
+    assert str(updated_task.performer) in tasks_list_content
