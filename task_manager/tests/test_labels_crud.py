e8ceb120310754aeb0cc6c872292cd1a988cd667
shitcoding <shitcoder@pm.me>
2022-10-22 00:49:26 +0300

    add test for label update

--- task_manager/tests/test_labels_crud.py
+++ task_manager/tests/test_labels_crud.py
@@ -47,3 +47,38 @@ def test_label_creation(client, auto_login_user, faker):
     assert (
         label.created_on.strftime("%d.%m.%Y %H:%M:%S") in labels_list_content
     )
+
+
+def test_label_update(
+    client,
+    create_label,
+    faker,
+    auto_login_user,
+):
+    """
+    Test updating label details.
+
+    Should change label details, redirect to labels list page
+    and show success flash message.
+    """
+    label = create_label()
+    new_name = faker.pystr(min_chars=10, max_chars=15)
+
+    client, _ = auto_login_user()
+    response = client.post(
+        reverse("label_update", kwargs={"pk": label.pk}),
+        {"name": new_name},
+    )
+    # Should redirect to labels list page
+    assert response.status_code == 302
+    assert response.url == reverse("labels")
+    # Should show success flash message after redirect
+    redirect_response = client.get(response.url)
+    assert "Label updated successfully" in str(
+        redirect_response.content,
+    )
+    # Label details should be changed successfully
+    updated_label = Label.objects.get(pk=label.pk)
+    assert updated_label.name == new_name
+    # Created label should be shown on the labels list page
+    assert updated_label.name in str(redirect_response.content)
