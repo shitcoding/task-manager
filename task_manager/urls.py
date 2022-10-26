14ccc2273deb3d6e37b2277f3dda9420109c47de
shitcoding <shitcoder@pm.me>
2022-10-26 16:57:00 +0300

    add language switcher to navbar

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from task_manager import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.IndexView.as_view(), name="index"),
+    # Localization
+    path("i18n/", include("django.conf.urls.i18n")),
     # Auth
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
