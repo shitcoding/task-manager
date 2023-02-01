07b93026a09c52326119f9c305adf9ea25f324a2
shitcoding <shitcoder@pm.me>
2023-02-01 13:24:14 +0300

    use django-environ to handle env credentials

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -3,9 +3,11 @@ import os
 from pathlib import Path
 
 import dj_database_url
+import environ
 from django.utils.translation import gettext_lazy as _
 
-from task_manager.utils import misc
+env = environ.Env()
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,18 +24,41 @@ logging.basicConfig(
 logging.getLogger("faker").setLevel(logging.ERROR)
 
 
-DEBUG = os.getenv("DJANGO_DEBUG", "true").lower() in {"yes", "1", "true"}
+DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
 if DEBUG:
-    SECRET_KEY = os.getenv("DJANGO_SECRET_KEY") or "wow-so-secret"
+    SECRET_KEY = env("DJANGO_SECRET_KEY", default="wow-so-secret")
     ALLOWED_HOSTS = ["*"]
 else:
-    SECRET_KEY = misc.getenv("DJANGO_SECRET_KEY")
-    ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+    SECRET_KEY = env("DJANGO_SECRET_KEY")
+    ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
+
 
 # HTTPS / SSL settings
-if os.getenv("LETSENCRYPT_HOST", None):
-    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# if os.getenv("LETSENCRYPT_HOST", None):
+#     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
+SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
+SESSION_COOKIE_SECURE = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
+CSRF_COOKIE_SECURE = True
+# https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
+# TODO: set this to 60 seconds first and then to 518400 once you prove the former works
+SECURE_HSTS_SECONDS = 60
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
+    "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-preload
+SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
+# https://docs.djangoproject.com/en/dev/ref/middleware/#x-content-type-options-nosniff
+SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
+    "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True
 
 
 INSTALLED_APPS = [
