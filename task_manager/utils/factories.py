0240c7be243368b619c0723159afa476d1b4033a
shitcoding <shitcoder@pm.me>
2023-03-24 17:28:16 +0300

    add factories for tasks, statuses, labels + refactor the code

--- task_manager/utils/factories.py
+++ task_manager/utils/factories.py
@@ -1,16 +1,26 @@
 from datetime import timezone
+from random import randrange
 
 import factory
-import faker.config
 from factory.django import DjangoModelFactory
 from override_autonow import override_autonow
 
 from task_manager.models import Label, SiteUser, Status, Task
 
-faker.config.DEFAULT_LOCALE = "en_US"
+
+class OverrideAutoNowAddMixin:
+    """
+    Mixin for overriding Django's auto_now_add parameter
+    when creating an object using factory.
+    """
+
+    @classmethod
+    @override_autonow
+    def _create(cls, model_class, *args, **kwargs):
+        return super()._create(model_class, *args, **kwargs)
 
 
-class SiteUserFactory(DjangoModelFactory):
+class SiteUserFactory(OverrideAutoNowAddMixin, DjangoModelFactory):
     class Meta:
         model = SiteUser
 
@@ -19,11 +29,42 @@ class SiteUserFactory(DjangoModelFactory):
     last_name = factory.Faker("last_name")
     signup_date = factory.Faker("date_time_this_year", tzinfo=timezone.utc)
 
-    @classmethod
-    @override_autonow  # Overriding Django's auto_now_add parameter
-    def _create(cls, model_class=SiteUser, *args, **kwargs):
-        return super(SiteUserFactory, cls)._create(
-            model_class,
-            *args,
-            **kwargs,
-        )
+
+class LabelFactory(OverrideAutoNowAddMixin, DjangoModelFactory):
+    class Meta:
+        model = Label
+
+    name = factory.Faker("text", max_nb_chars=20)
+    created_on = factory.Faker("date_time_this_year", tzinfo=timezone.utc)
+
+
+class StatusFactory(OverrideAutoNowAddMixin, DjangoModelFactory):
+    class Meta:
+        model = Status
+
+    name = factory.Faker("text", max_nb_chars=20)
+    created_on = factory.Faker("date_time_this_year", tzinfo=timezone.utc)
+
+
+class TaskFactory(OverrideAutoNowAddMixin, DjangoModelFactory):
+    class Meta:
+        model = Task
+
+    name = factory.Faker("text", max_nb_chars=30)
+    description = factory.Faker("text", max_nb_chars=1000)
+    created_on = factory.Faker("date_time_this_year", tzinfo=timezone.utc)
+    creator = factory.SubFactory(SiteUserFactory)
+    performer = factory.SubFactory(SiteUserFactory)
+    status = factory.SubFactory(StatusFactory)
+
+    # Handle many-to-many relation of `label` field
+    @factory.post_generation
+    def label(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            for label in extracted:
+                self.label.add(label)
+        else:
+            for _ in range(randrange(1, 4)):
+                self.label.add(LabelFactory())
